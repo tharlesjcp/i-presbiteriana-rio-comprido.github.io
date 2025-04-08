e97ae2b13a8cdedc61f985b069305de8f3c8b276
Tharles Corrêa <tharlesjcp12@gmail.com>
2025-04-08 19:52:19 -0300

    Update layout.js

--- adm/layout.js
+++ adm/layout.js
@@ -1,7 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
-  fetch("includes/header.html")
+  const path = window.location.pathname;
+  const basePath = path.includes("/adm/") ? "includes/header.html" : "adm/includes/header.html";
+
+  fetch(basePath)
     .then(response => response.text())
     .then(data => {
       document.body.insertAdjacentHTML("afterbegin", data);
-    });
+    })
+    .catch(error => console.error("Erro ao carregar header:", error));
 });
